ab08ff0a2ffc09603662aa760b1eeb08a09bf0ba
Rafale Alfardean <rgkminecraft6000@gmail.com>
2026-09-18 20:36:33 +0700

    Looping Task 1 & 2

--- src/04-looping/tasks/task-01.ts
+++ src/04-looping/tasks/task-01.ts
@@ -15,3 +15,9 @@
  * 3. After the loop finishes, display: "Attendance verification completed"
 
  */
+
+for (let studentNumber = 1; studentNumber <= 30; studentNumber++) {
+    console.log(`Checking attendance for Student #${studentNumber}`);
+}
+
+console.log("Attendance verification completed.");
